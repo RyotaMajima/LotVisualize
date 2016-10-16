3678114898ae9d34fb3299c8e9390f1662de979c
ryota majima <ryota.0571@gmail.com>
2016-10-16 22:23:14 +0900

    [add] func start, end, update to class Process

--- LotVisualize/Header.h
+++ LotVisualize/Header.h
@@ -2,7 +2,6 @@
 
 #include "define.h"
 
-
 class Lot{
 private:
     static int NUM;
@@ -17,7 +16,6 @@ public:
 public:
     Lot(); //constructor
     void showData(); //show lot state
-    void start(Process prc); //lot starting for some process
 };
 
 int Lot::NUM = 0;
@@ -37,9 +35,9 @@ void Lot::showData(){
     cout << boolalpha << nowProcess << endl;
 }
 
-void start(Process prc){
-
-}
+//void start(Process prc){
+//
+//}
 
 class Process{
 public:
@@ -51,6 +49,9 @@ public:
 
 public:
     Process(string _name, int _processTime); //constructor
+    void lotStart();
+    void lotEnd();
+    void update();
 };
 
 Process::Process(string _name, int _processTime){
@@ -59,4 +60,14 @@ Process::Process(string _name, int _processTime){
     isUsed = false;
     currentLot = 0;
     time = 0;
+}
+
+int searchLot(vector<Lot> &lots, Process prc){
+    for (int i = 0; i < N; i++){
+        if (lots[i].next == prc.name){
+            return i;
+        }
+    }
+
+    return -1;
 }
