df1715e2cb108e519fb706c3839b3e58763d8bb8
ryota majima <ryota.0571@gmail.com>
2016-10-21 19:10:10 +0900

    [delete] global var INFO

--- LotVisualize/define.h
+++ LotVisualize/define.h
@@ -13,8 +13,3 @@ using namespace std;
 const int T = 70;
 const int timeStep = 5;
 const int N = 5;
-
-const vector<tuple<string, int, int>> INFO{
-        make_tuple("DB", 2, 15), make_tuple("CURE", 1, 30),
-        make_tuple( "WB", 2, 10), make_tuple("RESIN", 4, 50),
-        make_tuple("CURE", 1, 45) };
