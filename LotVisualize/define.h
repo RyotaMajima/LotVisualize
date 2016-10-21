c7bd32e1bcc5b687d5e53d15665dc1e01ba46036
ryota majima <ryota.0571@gmail.com>
2016-10-21 10:42:19 +0900

    [del] global var NAME

--- LotVisualize/define.h
+++ LotVisualize/define.h
@@ -14,7 +14,6 @@ const int T = 70;
 const int timeStep = 5;
 const int N = 4;
 
-const vector<string> NAME{ "DB", "CURE", "WB", "RESIN", "CURE"};
 const vector<tuple<string, int>> INFO{
         make_tuple("DB", 15), make_tuple("CURE", 30),
         make_tuple( "WB", 10), make_tuple("RESIN", 50),
