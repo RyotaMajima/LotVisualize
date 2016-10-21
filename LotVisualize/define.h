ed16a0aae431d405bd953da0ef0e97acc34d02e1
ryota majima <ryota.0571@gmail.com>
2016-10-21 10:33:13 +0900

    [add] vector of tuple INFO

--- LotVisualize/define.h
+++ LotVisualize/define.h
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <string>
 #include <iomanip>
 
@@ -14,3 +15,7 @@ const int timeStep = 5;
 const int N = 4;
 
 const vector<string> NAME{ "DB", "CURE", "WB", "RESIN", "CURE"};
+const vector<tuple<string, int>> INFO{
+        make_tuple("DB", 15), make_tuple("CURE", 30),
+        make_tuple( "WB", 10), make_tuple("RESIN", 50),
+        make_tuple("CURE", 45) };
