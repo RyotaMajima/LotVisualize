78c10e715483b8b571cf158763f23cbc8d46cb6a
ryota majima <ryota.0571@gmail.com>
2016-10-23 12:02:28 +0900

    [modify] include header

--- LotVisualize/define.h
+++ LotVisualize/define.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
+#include <iomanip>
+#include <string>
 #include <vector>
 #include <iterator>
 #include <utility>
 #include <algorithm>
-#include <string>
-#include <iomanip>
 
 using namespace std;
 
-const int T = 100;
+const int T = 50;
 const int timeStep = 5;
-const int N = 10;
+const int N = 5;
