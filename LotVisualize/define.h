07c12185153978d32f76c721c5251029f681c723
ryota majima <ryota.0571@gmail.com>
2016-11-01 01:39:39 +0900

    [add] global var Tag and num

--- LotVisualize/define.h
+++ LotVisualize/define.h
@@ -17,3 +17,10 @@ using namespace std;
 const int T = 100;
 const int timeStep = 1;
 const int N = 10;
+
+const vector<string> ProcessName{
+    "DB", "DB_CURE", "WB", "RESIN", "RESIN_CURE",
+    "PLASMA", "MOLD", "MARK", "DICING", "DIC_CURE",
+    "TPG"};
+const vector<int> NumOfMachine{
+    2, 2, 2, 4, 4, 1, 2, 1, 2, 1, 2 };
