ac4dc3f936f0ae88ae8f91b6eb981d16856b38ad
ryota majima <ryota.0571@gmail.com>
2016-11-01 15:53:22 +0900

    [add] Tag class

--- LotVisualize/Tag.cpp
+++ LotVisualize/Tag.cpp
@@ -1,1 +1,12 @@
 #include "header.h"
+
+Tag::Tag() = default;
+
+Tag::Tag(string _thisName, string _nextName)
+    :thisName(_thisName), nextName(_nextName){}
+
+Tag& Tag::operator=(const Tag& rhs){
+    thisName = rhs.nextName;
+    nextName = rhs.nextName;
+    return *this;
+}
--- LotVisualize/Tag.h
+++ LotVisualize/Tag.h
@@ -5,4 +5,8 @@ private:
     string thisName;
     string nextName;
 
+public:
+    Tag();
+    Tag(string _thisName, string _nextName);
+    Tag& operator=(const Tag& tag);
 };
