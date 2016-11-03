276e32716006b20ce2a8812b6ca39bea0eaeedf5
ryota majima <ryota.0571@gmail.com>
2016-11-03 10:48:27 +0900

    [add] InProcess.h

---
+++ LotVisualize/InProcess.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "header.h"
+
+class InProcess{
+public:
+    string name;
+    int num;
+    Position pos;
+
+public:
+    InProcess(string _name, Position _pos);
+    
+
+
+};
